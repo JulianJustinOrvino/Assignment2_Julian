62b10864df5617a541811637fc89ea4a29182141
JulianJustinOrvino <112453974+JulianJustinOrvino@users.noreply.github.com>
2022-09-28 07:00:29 +0700

    Update views.py

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from mywatchlist.models import MyWatchListItem
+
+from mywatchlist.models import myWatchListItem
 from django.http import HttpResponse
 from django.core import serializers
 
 # Create your views here.
 def show_mywatchlist(request):
-    data_watchlist = MyWatchListItem.objects.all()
-    times = 0
+    data_watchlist = myWatchListItem.objects.all()
+    berapa_kali = 0
+    belum_nonton = 0
     for x in data_watchlist:
-        if("yes" == x.watched):
-            times += 1
-    if(times > 4):
+        if x.watched =="yes":
+            berapa_kali += 1
+            if x.watched == "no":
+                belum_nonton+=1
+    if(berapa_kali >= belum_nonton):
         msg_bonus = "Congratulations, you have watched movies a lot!"
     else:
         msg_bonus = "Woah, you have not much watched movies!"
@@ -18,25 +22,26 @@ def show_mywatchlist(request):
     context = {
         'list_item': data_watchlist,
         'name': 'Julian Justin Orvino',
-        'NPM': '2106720903',
+        'npm': '2106720903',
         'msg_bonus': msg_bonus,
     }
     return render(request, "mywatchlist.html", context)
 
 def show_xml(request):
-    data = MyWatchListItem.objects.all()
+    data = myWatchListItem.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 
 def show_json(request):
-    data = MyWatchListItem.objects.all()
+    data = myWatchListItem.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_json_by_id(request,id):
-    data = MyWatchListItem.objects.filter(pk=id)
+    data = myWatchListItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
 def show_xml_by_id(request,id):
-    data = MyWatchListItem.objects.filter(pk=id)
+    data = myWatchListItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
